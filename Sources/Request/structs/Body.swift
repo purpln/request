5c98c8012b036a6c4a6b0ffaa307eaa43f122cb1
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-03-02 18:19:05 +0300

    no message

--- Sources/Request/structs/Body.swift
+++ Sources/Request/structs/Body.swift
@@ -15,11 +15,12 @@ public extension Body {
 extension Body {
     public init(data: [UInt8], file: String, name: String, type: String = "image/jpeg") {
         let uuid: String = .uuid
-        var data: [UInt8] = []
-        data.append(contentsOf: [UInt8]("\r\n--\(uuid)\r\n".utf8) )
-        data.append(contentsOf: [UInt8]("Content-Disposition: form-data; name=\"\(name)\"; filename=\"\(file)\"\r\n".utf8) )
-        data.append(contentsOf: [UInt8]("Content-Type: \(type)\r\n\r\n".utf8) )
-        data.append(contentsOf: [UInt8]("\r\n--\(uuid)--\r\n".utf8) )
-        self.init(data: data, boundary: uuid)
+        var value: [UInt8] = []
+        value.append(contentsOf: [UInt8]("\r\n--\(uuid)\r\n".utf8) )
+        value.append(contentsOf: [UInt8]("Content-Disposition: form-data; name=\"\(name)\"; filename=\"\(file)\"\r\n".utf8) )
+        value.append(contentsOf: [UInt8]("Content-Type: \(type)\r\n\r\n".utf8) )
+        value.append(contentsOf: data)
+        value.append(contentsOf: [UInt8]("\r\n--\(uuid)--\r\n".utf8) )
+        self.init(data: value, boundary: uuid)
     }
 }
