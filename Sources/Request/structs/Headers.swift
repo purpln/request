476d5c1e591a0dac62bccf8f391f541a41de1eb4
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-03-02 16:17:35 +0300

    no message

--- Sources/Request/structs/Headers.swift
+++ Sources/Request/structs/Headers.swift
@@ -1,19 +1,17 @@
 public struct Headers: ExpressibleByDictionaryLiteral {
-    var value: [String: String]
+    public var value: [String: String]
     
-    mutating func updateCount(_ n: Int, for element: String) {
-        print(n, element)
+    public init(dictionaryLiteral elements: (String, String)...) {
+        self.init()
+        elements.forEach { tuple in update(tuple) }
     }
     
     public init(value: [String: String] = [:]) {
         self.value = value
     }
     
-    public init(dictionaryLiteral elements: (String, Int)...) {
-        self.init()
-        elements.forEach { (element, count) in
-            updateCount(count, for: element)
-        }
+    mutating func update(_ tuple: (key: String, value: String)) {
+        print(tuple.key, tuple.value)
     }
 }
 
